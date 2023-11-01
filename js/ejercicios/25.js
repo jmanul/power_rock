b75eed186602badf7ceee12d9781f3f7d2d687a6
José Manuel Sánchez <jmanul77@gmail.com>
2023-11-01 23:43:43 +0100

    ejercicio 26

---
+++ js/ejercicios/25.js
@@ -0,0 +1,21 @@
+
+
+// Usa un bucle para sumar el total de las ventas(sellCount)
+// de todos los productos.
+
+const products = [
+     { name: "Funko Dr. Strange", sellCount: 10 },
+     { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
+     { name: "Sable laser FX", sellCount: 23 },
+     { name: "Varita de Voldemort", sellCount: 6 },
+];
+
+let total = 0;
+
+for (let i = 0; i < products.length; i++) {
+
+     total = total + products[i].sellCount;
+
+}
+
+console.log(`el total de sellcounts es ${total}`);
