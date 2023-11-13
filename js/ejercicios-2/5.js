b308b87cd7708a4e47b6336ad0f18e7ee1bf44dd
José Manuel Sánchez <jmanul77@gmail.com>
2023-11-13 22:28:23 +0100

    5 primera parte

--- js/ejercicios-2/5.js
+++ js/ejercicios-2/5.js
@@ -104,3 +104,58 @@ const streamers2 = [
      { name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us' }
 ];
 
+const Ru = streamers2.filter((stream) => {
+
+     return stream.name === 'Rubius';
+
+})
+
+console.log(Ru);
+
+const i = streamers2.filter((stream) => {
+
+     return stream.name === 'Rubius' || stream.name === 'Ibai';
+
+})
+
+console.log(i);
+
+
+
+
+
+const toFilterStreamers = ((busqueda) => {
+
+
+     if (busqueda === 'Ru') {
+          
+          alert(Ru);
+          console.log(Ru);
+     } else if (busqueda === 'i') {
+          
+          console.log(i);
+     } else {
+
+          console.log('no hay resultados')
+
+     }
+
+
+})
+
+toFilterStreamers('i');
+
+// 5.7 Dado el siguiente html y javascript, utiliza.filter() para mostrar por consola
+// los streamers que incluyan la palabra introducida en el input.De esta forma, si
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'.Si introduzco 'i',
+//      me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+// En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
+
+const streamers3 = [
+     { name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft' },
+     { name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends' },
+     { name: 'Reven', age: 43, gameMorePlayed: 'League of Legends' },
+     { name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us' }
+];
+
+
