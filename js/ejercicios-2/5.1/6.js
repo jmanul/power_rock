59b71e27a7d5aaa88cc9c2ab46328ee9b5397677
José Manuel Sánchez <jmanul77@gmail.com>
2023-11-14 22:54:43 +0100

    ejercicio 6

--- js/ejercicios-2/5.1/6.js
+++ js/ejercicios-2/5.1/6.js
@@ -1,15 +1,25 @@
 
 
 // 6.1 Dado el siguiente array, usa.find() para econtrar el número 100.
-// const numbers = [32, 21, 63, 95, 100, 67, 43];
+
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+
+const numberIs = numbers.find((num) => num === 100);
+
+console.log(numberIs);
+ 
 // 6.2 Dado el siguiente array, usa.find() para econtrar la pelicula del año 2010.
-ç
+
 const movies = [
      { title: 'Madagascar', stars: 4.5, date: 2015 },
      { title: 'Origen', stars: 5, date: 2010 },
      { title: 'Your Name', stars: 5, date: 2016 }
 ];
 
+const movieIS = movies.find((movie) => movie.date === 2010 )
+
+console.log({ movieIS });
+console.log(`la pelicula de ${movieIS.date} es ${movieIS.title}`);
 
 // 6.3 Dado el siguiente javascript, usa.find() para econtrar el alien de nombre
 // 'Cucushumushu' y la mutación 'Porompompero'.Una vez que los encuentres, usa
@@ -22,7 +32,34 @@ const aliens = [
      { name: 'Cucushumushu', planet: 'Marte', age: 503021 }
 ];
 
+
+
+
 const mutations = [
      {name: 'Porompompero', description: 'Hace que el alien pueda adquirirla habilidad de tocar el tambor'},
 {name: 'Fly me to the moon', description: 'Permite volar,solo y exclusivamente a la luna'},{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
 ];
+
+// ***** esta funcion se puede simplicar como en el caso de abajo *****
+
+// const alienIs = aliens.find((alien) => {
+
+//      return alien.name === 'Cucushumushu';
+// });
+
+const alienIs = aliens.find((alien) => alien.name === 'Cucushumushu');
+
+const mutationIs = mutations.find((mutation) => mutation.name === 'Porompompero');
+
+
+const aliensMutations = {
+
+     ...alienIs,
+     mutation: {
+
+          ...mutationIs
+     }
+};
+
+console.log(aliensMutations)
+
